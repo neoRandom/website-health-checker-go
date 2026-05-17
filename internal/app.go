b013d7e04fb820d7a82ffc26c5d3a25385a6f201
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-05-17 18:09:49 -0300

    feat: add Templ, HTMX, and Tailwind CSS to the project

--- internal/app.go
+++ internal/app.go
@@ -1,10 +1,23 @@
 package internal
 
 import (
-	"fmt"
+	"http-server/template"
 	"net/http"
 	"time"
+	"github.com/a-h/templ"
+	"github.com/alexedwards/scs/v2"
 )
+
+//
+
+type GlobalState struct {
+	Count int
+}
+
+var global GlobalState
+var sessionManager *scs.SessionManager
+
+//
 
 type App struct {
 	Addr              string
@@ -14,16 +27,46 @@ type App struct {
 	IdleTimeout       time.Duration
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, internet!</h1>")
-}
-
 func (a *App) GetServer() *http.Server {
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handlerFunc)
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			r.ParseForm()
 
-	wMux := WrapAllMiddleware(mux)
+			// Check to see if the global button was pressed.
+			if r.Form.Has("global") {
+				global.Count++
+			}
+			if r.Form.Has("session") {
+				currentCount := sessionManager.GetInt(r.Context(), "count")
+				sessionManager.Put(r.Context(), "count", currentCount+1)
+			}
+		}
+
+		sessionCount := sessionManager.GetInt(r.Context(), "count")
+		component := template.Index(global.Count, sessionCount)
+		component.Render(r.Context(), w)
+	}))
+
+	mux.Handle("/hello", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		name := r.FormValue("name")
+		component := template.Hello(name)
+		component.Render(r.Context(), w)
+	}))
+
+	mux.Handle("/404", templ.Handler(template.NotFound(), templ.WithStatus(http.StatusNotFound)))
+
+	sMux := sessionManager.LoadAndSave(mux)
+	wMux := WrapAllMiddleware(sMux)
 
 	srv := &http.Server{
 		Addr:              a.Addr,
