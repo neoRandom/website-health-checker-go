aa6ffe1fdb7871b89f7c6c9d0c1172963bbecdd4
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-06-19 18:00:50 -0300

    chore: move main.go to cmd/server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"http-server/database"
+	"http-server/internal/database"
 	"http-server/internal/adapter/driven"
 	"http-server/internal/adapter/driver"
 	"http-server/internal/config"
-	usecases "http-server/internal/use_cases"
+	"http-server/internal/use_cases"
 	"log"
 
 	_ "modernc.org/sqlite"
